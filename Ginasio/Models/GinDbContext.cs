e09398255351e5b3ee94b7e38e0b77db6b654d54
Rushi10006 <160591260+Lucas10006@users.noreply.github.com>
2025-07-09 01:37:36 +0100

    Add project files.

---
+++ Ginasio/Models/GinDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ginasio.Models
+{
+    public class GinDbContext : DbContext
+    {
+        public GinDbContext(DbContextOptions<DbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+            
+            
+        
+    }
+}
