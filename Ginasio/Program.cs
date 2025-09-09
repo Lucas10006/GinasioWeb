ed70f3ad16ec6a05c310d658d38907e545a43ce3
Rushi10006 <160591260+Lucas10006@users.noreply.github.com>
2025-09-09 18:21:44 +0100

    Mudanças para a database

--- Ginasio/Program.cs
+++ Ginasio/Program.cs
@@ -1,4 +1,10 @@
+using Ginasio.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<GinDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("GymDb")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
